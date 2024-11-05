cb451625180b7b36027cd545b8b119221362dbf1
aarvros <awclose02@gmail.com>
2024-11-05 17:27:10 -0600

    added release folder, created post publish script, fixed readme

---
+++ PostPublishCompress.ps1
@@ -0,0 +1,36 @@
+$workingDir = $PWD.Path
+$publishDir = "$workingDir\bin\Release\net8.0-windows\win-x64\publish"
+$objDir = "$publishDir\bin"
+$exeFile = "$publishDir\StatementMover.exe"
+$readmeFile = "$publishDir\README.txt"
+$shortcutFile = "$publishDir\StatementMover.exe.lnk"
+$zipFile = "release\StatementMover.zip"
+
+if (Test-Path $objDir) {
+    Remove-Item -Path $objDir -Recurse -Force
+}
+
+if (-not (Test-Path $objDir)) {
+    New-Item -ItemType Directory -Path $objDir
+}
+
+$nlFile = "$publishDir\nl\PdfiumViewer.resources.dll"
+Move-Item $nlFile -Destination "$objDir\$_"
+$x64File = "$publishDir\x64\pdfium.dll"
+Move-Item $x64File -Destination "$objDir\$_"
+if (Test-Path "$publishDir\nl") {
+    Remove-Item -Path "$publishDir\nl" -Force
+}
+if (Test-Path "$publishDir\x64") {
+    Remove-Item -Path "$publishDir\x64" -Force
+}
+
+Get-ChildItem -Path $publishDir -File | Where-Object { $_.Name -ne 'README.txt' } | ForEach-Object {
+    Move-Item $_.FullName -Destination "$objDir\$_"
+}
+
+if (Test-Path $zipFile) {
+    Remove-Item $zipFile -Force
+}
+Add-Type -AssemblyName "System.IO.Compression.FileSystem"
+[System.IO.Compression.ZipFile]::CreateFromDirectory($publishDir, $zipFile)
--- README.txt
+++ README.txt
@@ -4,7 +4,8 @@ Getting Started:
 2. The installer will be automatically downloaded to your downloads folder
 3. Run the installer. The installer may ask you to enter admin credentials
 4. Extract the zip file to a known location
-5. Right click on the EXE and click create shortcut
-6. Rename the shortcut to a name of your choosing
-7. Move the shortcut to your desktop or another location where you would like to run the application from.
-8. Double click on the shortcut you created to run the application
+5. Double click on the bin folder and locate the StatementMover.exe
+6. Right click on the EXE and click create shortcut
+7. Rename the shortcut to a name of your choosing
+8. Move the shortcut to your desktop or another location where you would like to run the application from.
+9. Double click on the shortcut you created to run the application
